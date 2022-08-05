917dd73a7e5262aab3cbad28868d9df83efc3c67
pachara171 <pachara1045@gmail.com>
2022-08-05 19:32:44 +0700

    commit WordCard.ja (more)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
 import './App.css';
-const word = "Hello";
 function App() {
   return (
     <div>
       {
-        Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+        <WordCard value="hello"/>
       }
     </div>
   );
