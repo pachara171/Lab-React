edad333d2ef6e5abc5da0f45229b822f4ad4ed1e
pachara171 <pachara1045@gmail.com>
2022-08-19 19:16:12 +0700

    1.Logic -add button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React,{useState} from 'react';
 import WordCard from './WordCard';
 import './App.css';
 function App() {
+  const [view, setView] = useState("เริ่มนับเวลา")
+
+
   return (
-    <div>
-      {
-        <WordCard value="hello"/>
-      }
+    <div className='container'>
+      <div className='box'>
+        <div className='time-button' onClick={() =>{
+          setView("เริ่มนับเวลา")
+        }}>
+          {view}
+        </div>
+        {
+          <WordCard value="hello"/>
+        }
+        
+      </div>
     </div>
   );
 }
